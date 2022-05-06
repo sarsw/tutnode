c4dd2d3ec15721b86a4e0e5a7ea248a0e97c0418
Steve Russell <steve.russell@swintontechnology.com>
2022-05-06 12:45:29 +0100

    Playing around with npm and nodemon. 'npm run dev' will start the app with this config

--- app.js
+++ app.js
@@ -13,6 +13,8 @@
 //  it exists in the root folder of the project & can be created manually
 // npm init (a wizard to create the project template)
 // npm init -y (accept all defaults)
+// npm i nodemon -D (or --save-dev) install and save as adev dependency
+// npm run dev (can be used to run a dev scripts usinf nodemon, e.g.     "dev": "nodemon app.js")
 
 const _ = require('lodash')
 
@@ -20,5 +22,5 @@ const data = [1,[2,[3,[4]]]]
 
 const newData = _.flattenDeep(data)
 
-console.log((data));
+console.log(data);
 console.log((newData));
