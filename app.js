4e5f57b48e934f799e2331c1f4f21390e605b771
Steve Russell <steve.russell@swintontechnology.com>
2022-05-12 13:03:22 +0100

    Simple event emitter with multiple listeners

--- app.js
+++ app.js
@@ -6,4 +6,9 @@ customEmitter.on('response', ()=>{
     console.log('data rx');
 })
 
-customEmitter.emit('response')
+customEmitter.on('response', ()=>{
+    console.log('data rx again');
+})
+
+
+customEmitter.emit('response') // got to emit after you've registered the listeners
