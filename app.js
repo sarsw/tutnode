82571d02c70ab6b54fafbe54521bda1eb7ee45ff
Steve Russell <steve.russell@swintontechnology.com>
2022-05-12 12:54:33 +0100

    Intro into Promises and await with readFile and writeFile

--- app.js
+++ app.js
@@ -1,30 +1,41 @@
-const {readFile} = require('fs');
+const {readFile, writeFile} = require('fs').promises
+// const util = require('util')
 
-const getText = (path) => {
-    return new Promise((resolve, reject)=>{
-        readFile(path, 'utf8', (err, data) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(data)
-            }
-        })
-    })
-}
+// const readFilePromise = util.promisify(readFile)
+// const writeFilePromise = util.promisify(writeFile)
+
+
 
 console.log('start');
 
-// getText('./content/first.txt')
-//     .then((result)=>console.log(result))
-//     .catch((err) => console.log(err))
 
 const start = async() => {
     try {
-            const first = await getText('./content/first.txt')
-            const second = await getText('./content/second.txt')
-            console.log(first, second);
+        // const first = await readFilePromise('./content/first.txt', 'utf-8')
+        // const second = await readFilePromise('./content/second.txt', 'utf-8')
+        const first = await readFile('./content/first.txt', 'utf-8')
+        const second = await readFile('./content/second.txt', 'utf-8')
+        // await writeFilePromise('./content/result-mind-bomb.txt', `this is awesom:${first} ${second}`)
+        await writeFile('./content/result-mind-bomb.txt', `this is awesom:${first} ${second}`, {flag: 'a'})
+        console.log(first, second);
     } catch (error) {
         console.log(error);
     }
 }
 start()
+
+// getText('./content/first.txt')
+//     .then((result)=>console.log(result))
+//     .catch((err) => console.log(err))
+
+// const getText = (path) => {
+//     return new Promise((resolve, reject)=>{
+//         readFile(path, 'utf8', (err, data) => {
+//             if (err) {
+//                 reject(err);
+//             } else {
+//                 resolve(data)
+//             }
+//         })
+//     })
+// }
