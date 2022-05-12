2ba12f0f130221f590439158c77ec58d3d3afdd3
Steve Russell <steve.russell@swintontechnology.com>
2022-05-12 13:01:32 +0100

    Simple event emitter example

--- app.js
+++ app.js
@@ -1,41 +1,9 @@
-const {readFile, writeFile} = require('fs').promises
-// const util = require('util')
+const EventEmitter = require('events')
 
-// const readFilePromise = util.promisify(readFile)
-// const writeFilePromise = util.promisify(writeFile)
+const customEmitter = new EventEmitter()
 
+customEmitter.on('response', ()=>{
+    console.log('data rx');
+})
 
-
-console.log('start');
-
-
-const start = async() => {
-    try {
-        // const first = await readFilePromise('./content/first.txt', 'utf-8')
-        // const second = await readFilePromise('./content/second.txt', 'utf-8')
-        const first = await readFile('./content/first.txt', 'utf-8')
-        const second = await readFile('./content/second.txt', 'utf-8')
-        // await writeFilePromise('./content/result-mind-bomb.txt', `this is awesom:${first} ${second}`)
-        await writeFile('./content/result-mind-bomb.txt', `this is awesom:${first} ${second}`, {flag: 'a'})
-        console.log(first, second);
-    } catch (error) {
-        console.log(error);
-    }
-}
-start()
-
-// getText('./content/first.txt')
-//     .then((result)=>console.log(result))
-//     .catch((err) => console.log(err))
-
-// const getText = (path) => {
-//     return new Promise((resolve, reject)=>{
-//         readFile(path, 'utf8', (err, data) => {
-//             if (err) {
-//                 reject(err);
-//             } else {
-//                 resolve(data)
-//             }
-//         })
-//     })
-// }
+customEmitter.emit('response')
