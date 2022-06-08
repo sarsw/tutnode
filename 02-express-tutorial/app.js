30642a21b4718cb271486cfaab0b4a8121c78217
Steve Russell <steve.russell@swintontechnology.com>
2022-06-08 14:01:02 +0100

    Added Express using 'npm install express --save' and added the basics of a server using the framework. Note you can explicitly send the response status by chaining with the send() method

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,55 +1,27 @@
-const http = require('http')
-const {readFileSync} = require('fs')
+const express = require('express')
+const app = express()
 
-// get all files
-const homePage = readFileSync('./navbar-app/index.html')   // read at startup
-const homeStyles = readFileSync('./navbar-app/styles.css')   // read at startup
-const homeImage = readFileSync('./navbar-app/logo.svg')   // read at startup
-const homeLogic = readFileSync('./navbar-app/browser-app.js')   // read at startup
+// app.get
+// app.post
+// app.put
+// app.delete
+// app.all
+// app.use
+// app.listen
+// ...
 
-const server = http.createServer((req,res)=>{
-
-    const url = req.url
-
-    console.log(`server hit ${url}`)
-
-    if (url === '/') {
-        res.writeHead(200, {
-            'Content-Type': 'text/html'
-          })
-        res.write(homePage)
-        res.end()
-    }
-    else if (url === '/styles.css') {
-        res.writeHead(200, {
-            'Content-Type': 'text/css'
-          })
-        res.write(homeStyles)
-        res.end()
-    }
-    else if (url === '/logo.svg') {
-        res.writeHead(200, {
-            'Content-Type': 'image/svg+xml'
-          })
-        res.write(homeImage)
-        res.end()
-    }
-    else if (url === '/browser-app.js') {
-        res.writeHead(200, {
-            'Content-Type': 'text/javascript'
-          })
-        res.write(homeLogic)
-        res.end()
-    }
-    else
-    {
-        res.writeHead(404, {
-            'Content-Type': 'text/html'
-          })
-        res.write('<h1>not found</h1>')
-        res.end()
-    }
+app.get('/', (req,res) => {
+    res.status(200).send('Home Page')
 })
 
+app.get('/about', (req,res) => {
+    res.status(200).send('About Page')
+})
 
-server.listen(5000)
+app.all('*', (req,res) => {
+    res.status(404).send('<h1>Not Found</h1>')
+})
+
+app.listen(5000, () => {
+    console.log("listening on port 5000");
+})
